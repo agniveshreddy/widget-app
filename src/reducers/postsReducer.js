da6f758f0dc3dc2fc87c046ceaeb927559a3e0ee
Agnivesh Reddy <m.agnivesh.reddy@gmail.com>
2020-05-07 09:34:44 +0200

    commit5: cleanup

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -2,7 +2,6 @@ import {types}  from '../actions';
 
 export default function(state = [], action) {
   const { response } = action;
-  console.log('action type: '+ action.type);
   switch(action.type) {
     case types.GET_POSTS_SUCESS:
       return [...state, ...response] ;
